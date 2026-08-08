062a3f5bdfc346b60d867f85b7cd1eb07f857d8f
Arsh Mishra <161199109+ArshMishra1@users.noreply.github.com>
2026-08-08 23:49:09 +0530

    introduction

---
+++ introduction/index.js
@@ -0,0 +1,22 @@
+//introduction-javascript is a lightweight interpreted and just in time compiled programming language used to make web pages interactive 
+
+/*console.log() - console.log() is inbuilt javaScript Object with have log() used to print the output , message , error in the browser console.
+return type of console.log() is undefined because it does not return any value. */
+
+console.log(typeof("arsh"));
+let a=console.log();
+console.log(typeof(a));
+
+//! Comment- comment are used to explain the code and it is a part of code that is ignored by the javascript engine during code execution. There are two types of comment in javascript
+// 1. Single line comment - 
+// 2. Multi line comment -
+
+// console.log("Hello World"); // single line comment
+/*
+console.log("hello world  1")
+console.log("hello world  1")   multi line comment
+console.log("hello world  1")
+console.log("hello world  1")
+*/
+
+
