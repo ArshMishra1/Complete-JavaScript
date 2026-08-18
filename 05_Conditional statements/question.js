df62cfa14423fd208cd55339e7c02434184565a5
Arsh Mishra <161199109+ArshMishra1@users.noreply.github.com>
2026-08-18 19:12:24 +0530

    Conditional _statements

---
+++ 05_Conditional statements/question.js
@@ -0,0 +1,206 @@
+// Create a simple ATM program.
+// User can choose:
+// 1. Check Balance
+// 2. Deposit
+// 3. Withdraw
+// 4. Exit
+// Note that in case of “Deposit” if deposit amount is less than 1Rs produce error otherwise deposit the amount and show
+// the message with a new balance. And in case of “Withdraw” if withdraw amount is greater than balance then or less
+// than 1Rs then produce error otherwise withdraw amount and show remaining balance.
+
+
+// let user;
+// let checkbalance=2333;
+// let deposit;
+// let withdraw;
+// if(user == deposit){
+//     if( deposit < 1){
+//         console.log("error  amount is less than 1Rs")
+//     }else{
+//         console.log(deposit)
+//     }
+// }if(user == withdraw){
+//     if((withdraw <1) &&(withdraw > 1)){
+//         console.log("error")
+//     }else{
+//         console.log(withdraw)
+//     }
+// }else{
+//     console.log(". Exit")
+// }
+
+
+
+
+
+//dRAK MODE AND WHITE MODE
+
+// let mode=prompt(" select or enter mode name");
+
+// if( mode==="Drak"){
+//     console.log("background color is brack or mode change inot color")
+// }else if(
+//     mode==="light"
+// ){
+//     console.log("background color is white or mode change int color")
+
+// }else{
+//     console.log("plasase select  mode or  or default")
+// }
+
+//Traffic Signal
+
+// Red    → Stop
+// Yellow → Ready
+// Green  → Go
+// Any other color → Invalid Signal
+
+//  let traficlight="Red"
+
+// switch(traficlight){
+//     case "Red":
+//     console.log("stop")
+//     break ; 
+//     case "Yellow":
+//         console.log("Ready")
+//     break;
+//     case "Go":
+//     console.log("GO GO")
+//     break;
+//     default :
+//     console.log("Invalid Signal")
+
+// }
+
+// Day Number
+
+// 1 → Monday
+// 2 → Tuesday
+// 3 → Wednesday
+// 4 → Thursday
+// 5 → Friday
+// 6 → Saturday
+// 7 → Sunday
+// Any other → Invalid Day
+
+// let Day = 2;
+// switch (Day) {
+//     case 1:
+//         console.log("Monday")
+//         break;
+//     case 2:
+//         console.log("Tuesday")
+
+//         break;
+//     case 3:
+//         console.log("Wednesday")
+
+//         break;
+//     case 4:
+//         console.log("Thursday")
+
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday")
+//         break;
+//     case 7:
+//         console.log("Sunday")
+//         break;
+//     default:
+//         console.log("InvalidDay")
+// }
+
+
+//
+// Calculator
+
+// User chooses:
+
+// 1 → Addition
+// 2 → Subtraction
+// 3 → Multiplication
+// 4 → Division
+// Default → Invalid Choice
+
+// Take:
+
+// let a = 20;
+// let b = 10;
+
+// let a = 20;
+// let b = 10;
+// let userChooses = 1;
+
+// switch (userChooses) {
+//     case 1:
+//         console.log(` {a} + {b}`)
+//         break;
+//     case 2:
+//         console.log(` {a} -{b}`)
+//         break; case 3:
+//         console.log(` {a} % {b}`)
+//         break; case 4:
+//         console.log(` {a}/ {b}`)
+//         break;
+//     default:
+//         console.log("nvalid Choice");
+// }
+
+
+// Restaurant Menu
+
+// 1 → Pizza
+// 2 → Momos
+// 3 → Momos
+// 4 → Pasta
+// 5 → Cold Drink
+// // Default → Item Not Available
+
+// let RestaurentMenu= 1;
+// switch(RestaurentMenu){
+//     case 1:
+//     console.log("Pizza");
+//     break;
+//     case 2:
+//     console.log("Momos");
+//     break;case 3:
+//     console.log("Momos");
+//     break;case 4:
+//     console.log("Pasta");
+//     break;case 5:
+//     console.log("Cold Drink");
+//     break;
+//     default:
+//         console.log(" Item Not Available");
+// }
+
+
+
+// Movie Ticket Price
+
+// Age Group
+
+// "child"  → ₹100
+// "adult"  → ₹200
+// "senior" → ₹150
+// Default → Invalid Age Group
+
+
+// let AgeGroup ="child"
+
+// switch(AgeGroup){
+//     case "child":
+//         console.log("100");
+//         break;
+//           case "adulr":
+//         console.log("200");
+//         break;  case "senior":
+//         console.log("150");
+//         break;
+//         default:
+//             console.log("Invalid Age Group")
+
+// } 
